c4d55ef0481b603d1347f4a4cf75ba32f2c7ffa4
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-02-23 12:23:49 -0500

    add splash screen

---
+++ app/src/main/java/gadolfolozano/pe/mvpexample/view/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package gadolfolozano.pe.mvpexample.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by gustavo.lozano on 2/23/2018.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int secondsDelayed = 2;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+
+    @Override
+    protected void bindViews() {
+        //Do nothing
+    }
+
+    @Override
+    protected void prepareActivity() {
+        //Do nothing
+    }
+
+    @Override
+    protected void initializeInjector() {
+        //Do nothing
+    }
+}
